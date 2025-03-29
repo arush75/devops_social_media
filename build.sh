edbd7916c4a1bf108334a7faecba8ef4f368e538
Arush <115131131+arush75@users.noreply.github.com>
2025-03-29 17:52:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd /root/ansible
+cd /root/ansible/myprojectdata
 
 # Build the Docker image locally
 docker build -t $JOB_NAME:$BUILD_ID .
 
 # Save the Docker image as a tarball for transfer
-docker save -o $JOB_NAME-$BUILD_ID.tar.gz $JOB_NAME:$BUILD_ID
+docker save -o /root/ansible/$JOB_NAME-$BUILD_ID.tar.gz $JOB_NAME:$BUILD_ID
 
 # Move Docker image to Docker VM
-scp -o StrictHostKeyChecking=no $JOB_NAME-$BUILD_ID.tar.gz root@192.168.106.129:/root/
+scp -o StrictHostKeyChecking=no /root/ansible/$JOB_NAME-$BUILD_ID.tar.gz root@192.168.106.129:/root/
