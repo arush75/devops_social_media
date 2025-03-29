08ebc45d5d3085cb674f1c34f4361a1526384bb9
Arush <115131131+arush75@users.noreply.github.com>
2025-03-29 17:49:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
+cd /root/ansible
+
+# Build the Docker image locally
 docker build -t $JOB_NAME:$BUILD_ID .
 
-docker tag $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:$BUILD_ID
+# Save the Docker image as a tarball for transfer
+docker save -o $JOB_NAME-$BUILD_ID.tar.gz $JOB_NAME:$BUILD_ID
 
-docker tag $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
-
-docker push jacksneel/$JOB_NAME:$BUILD_ID
-
-docker push jacksneel/$JOB_NAME:latest
-
-docker rmi -f $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
+# Move Docker image to Docker VM
+scp -o StrictHostKeyChecking=no $JOB_NAME-$BUILD_ID.tar.gz root@192.168.106.129:/root/
